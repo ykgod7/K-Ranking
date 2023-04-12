7aa8ca64869de81a88b97109411c878b8e1ce47d
Jihwan Shin <ejihwan7@gmail.com>
2023-04-12 23:43:10 +0900

    axios 수정

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     allowedHosts: "all",
-    proxy: 'http://52.79.78.43:8081'
+    proxy: 'http://localhost:8081'
   },
   css: {
     loaderOptions: {
