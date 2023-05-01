6395d587639fe45302a35991fc64a8fe9179611c
Jihwan Shin <ejihwan7@gmail.com>
2023-05-02 00:04:25 +0900

    request https로 수정

--- src/App.vue
+++ src/App.vue
@@ -9090,7 +9090,7 @@ export default {
         }
 
         const getData = async () => {
-            const res = await axios.get('http://k-ranking.co.kr:8081/api/universities')
+            const res = await axios.get('https://k-ranking.co.kr:8081/api/universities')
             store.commit('setUniversities', res.data.universities)
             store.commit('setMajors', res.data.majors)
             store.commit('setYears', [2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009])
