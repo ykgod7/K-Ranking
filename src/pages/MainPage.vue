e775ada4066563dd3a5924388ae4ef54a6badb07
Jihwan Shin <ejihwan7@gmail.com>
2023-05-01 23:56:41 +0900

    modal 1차 작업

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -2,8 +2,108 @@
   <div v-if="loading"></div>
   <div class="container" v-else>
 
-    <div v-if="openModal" class="modal">
-      <div class="modal-content"></div>
+    <div v-if="openModal" class="modal" data-backdrop="static" data-keyboard="false">
+      <div class="modal-content">
+        <div class="first-row">
+          <div class="logo">
+            <img class="logo" :src="require(`../assets/logo/서울대학교.png`)" />
+          </div>
+          <div class="university-name">
+            <div class="top-wrapper">
+              <div class="korean">서울대학교</div>
+              <span class="material-icons-outlined icon">map</span>
+            </div>
+            <div class="english">
+              Seoul National University
+            </div>
+          </div>
+          <span class="material-icons-round close-icon" @click="openModal = false">close</span>
+        </div>
+        <div class="second-row">
+          <div class="uni-description">
+            KAIST는 과학기술을 통한 경제발전이라는 정부의 목표 아래 국내 최초의 연구중심 KAIST는 과학기술을 통한 경제발전이라는 정부의 목표 아래 국내 최초의 연구중심KAIST는 과학기술을 통한
+            경제발전이라는 정부의 목표 아래 국내 최초의 연구중심 KAIST는 과학기술을 통한 경제발전이라는 정부의 목표 아래 국내 최초의 연구중심KAIST는 과학기술을 통한 부의 목표 아래 국내 최초의
+            연구중심KAIST는 과학기술을 통한 부의 목표 아래 국내 최초의 연구중심KAIST는 과학기술을 통한 부의 목표 아래 국내 최초의 연구중심KAIST는 과학기술을 통한 부의 목표 아래 국내 최초의
+            연구중심KAIST는 과학기술을 통한
+          </div>
+          <input type="checkbox" class="read-more" />
+        </div>
+
+        <div class="third-row">
+          <div class="line">
+            <div class="box">
+              <p class="filter-name"><span>총 학생수</span></p>
+              <p class="filter-value">6,011명</p>
+            </div>
+
+            <div class="box">
+              <p class="filter-name"><span>외국인 학생수</span></p>
+              <p class="filter-value">6,011명</p>
+            </div>
+            <div class="box">
+              <p class="filter-name"><span>신입생 경쟁률</span></p>
+              <p class="filter-value">1:3</p>
+            </div>
+            <div class="box">
+              <p class="filter-name"><span>학생/교수 비율</span></p>
+              <p class="filter-value">1:5</p>
+            </div>
+            <div class="box">
+              <p class="filter-name"><span>취업률</span></p>
+              <p class="filter-value">87%</p>
+            </div>
+          </div>
+          <div class="line">
+            <div class="box">
+              <p class="filter-name"><span>평균 등록금</span></p>
+              <p class="filter-value">6,011,785원</p>
+            </div>
+            <div class="box">
+              <p class="filter-name"><span>기숙사 수용률</span></p>
+              <p class="filter-value">6,011명</p>
+            </div>
+            <div class="box">
+              <p class="filter-name"><span>학생 1인당<br>연간 장학금</span></p>
+              <p class="filter-value">6,011,785원</p>
+            </div>
+            <div class="box">
+              <p class="filter-name"><span>학생 1인당<br>평균 교육비</span></p>
+              <p class="filter-value">6,011,785원</p>
+            </div>
+            <div class="box">
+              <p class="filter-name"><span>학생 1인당<br>평균 도서 자료수</span></p>
+              <p class="filter-value">34권</p>
+            </div>
+
+          </div>
+        </div>
+
+        <div class="fourth-row">
+          <div class="content-left">
+            <p class="major-title">학과명</p>
+            <div class="major-dropdown" @click="majorDropdown = !majorDropdown">
+              <div class="current">
+                <p>{{ selectedMajor }}</p>
+              </div>
+              <div class="dropdown-content" v-if="majorDropdown">
+                <div class="option" @click="selectedMajor = '전체'">전체</div>
+                <div class="option" v-for="major in majors" :key="major" @click="selectedMajor = major">{{ major }}</div>
+              </div>
+              <span class="material-icons-round icon">arrow_drop_down</span>
+            </div>
+          </div>
+          <div class="content-right">
+            <div class="wrapper">
+              <p class="title">신입생 모집 인원</p>
+              <div class="value"><span>4,344명</span></div>
+            </div>
+            <div class="wrapper">
+              <p class="title">졸업자 수</p>
+              <div class="value"><span>4,344명</span></div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div class="modal-dim" @click="openModal = false"></div>
     </div>
 
@@ -16,12 +116,18 @@
       <!-- 대학교 리스트 -->
       <div class="top-bar-wrapper">
         <div class="inner-wrapper">
-          <div class="filter rank">순위<span class="material-icons-round icon">north</span></div>
-          <div class="filter name">대학교명<span class="material-icons-round icon">north</span></div>
-          <div class="filter comp">입학 경쟁률<span class="material-icons-round icon">north</span></div>
-          <div class="filter num">총 학생수<span class="material-icons-round icon">north</span></div>
-          <div class="filter sf-ratio">학생/교수 비율<span class="material-icons-round icon">north</span></div>
-          <div class="filter tuition">등록금<span class="material-icons-round icon">north</span></div>
+          <div class="filter rank" @click="filterState = 1"><span class="border"
+              :class="filterState === 1 ? 'isActive' : null">순위</span></div>
+          <div class="filter name" @click="filterState = 2"><span class="border name"
+              :class="filterState === 2 ? 'isActive' : null">학교명</span></div>
+          <div class="filter comp" @click="filterState = 3"><span class="border"
+              :class="filterState === 3 ? 'isActive' : null">입학 경쟁률</span></div>
+          <div class="filter num" @click="filterState = 4"><span class="border"
+              :class="filterState === 4 ? 'isActive' : null">총 학생수</span></div>
+          <div class="filter sf-ratio" @click="filterState = 5"><span class="border"
+              :class="filterState === 5 ? 'isActive' : null">학생/교수 비율</span></div>
+          <div class="filter tuition" @click="filterState = 6"><span class="border"
+              :class="filterState === 6 ? 'isActive' : null">평균 등록금</span></div>
         </div>
       </div>
     </div>
@@ -52,7 +158,7 @@
 
 <script>
 
-import { ref, onBeforeMount, computed } from 'vue'
+import { ref, onBeforeMount, computed, watch } from 'vue'
 import { useRouter } from 'vue-router'
 import { useStore } from 'vuex'
 
@@ -69,6 +175,10 @@ export default {
     const openModal = ref(false)
     const store = useStore()
     const loading = ref(true)
+    const filterState = ref(null)
+    const majorDropdown = ref(false)
+    const selectedMajor = ref('전체')
+    const majors = ref(['경영학', '우주공학', '물리학', '화학', '생물학', '컴퓨터공학', '반도체', '신기술', '의학'])
 
     const universities = computed(() => {
       return store.getters.filterUniversity
@@ -78,7 +188,13 @@ export default {
       return val.toLocaleString();
     }
 
-
+    watch(openModal, (val) => {
+      if (val) {
+        document.body.style.overflow = "hidden"
+      } else {
+        document.body.style.overflow = "auto"
+      }
+    })
 
     onBeforeMount(() => {
       loading.value = false
@@ -132,6 +248,10 @@ export default {
       arrowDown,
       search,
       openModal,
+      filterState,
+      majors,
+      majorDropdown,
+      selectedMajor
     }
   },
 }
@@ -167,6 +287,265 @@ export default {
       height: 900px;
       border-radius: 20px;
       background: white;
+      padding: 40px 40px 70px 40px;
+
+      .first-row {
+        position: relative;
+        height: 60px;
+        display: flex;
+
+        .logo {
+          width: 60px;
+          height: 60px;
+        }
+
+        .university-name {
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          padding-left: 20px;
+          height: 100%;
+
+          .top-wrapper {
+            display: flex;
+            align-items: center;
+
+            .korean {
+              font-weight: 700;
+              font-size: 30px;
+              color: #222222;
+              line-height: 36px;
+              padding-right: 6px;
+            }
+
+            .icon {
+              cursor: pointer;
+              height: max-content;
+              font-size: 25px;
+            }
+          }
+
+          .english {
+            font-size: 16px;
+            color: #bababa;
+            font-weight: 500;
+            line-height: 19px;
+          }
+        }
+
+        .close-icon {
+          cursor: pointer;
+          position: absolute;
+          top: 0;
+          right: 0;
+          font-size: 32px;
+        }
+      }
+
+      .second-row {
+        position: relative;
+
+        .uni-description {
+          padding-top: 30px;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          display: -webkit-box;
+          -webkit-line-clamp: 3;
+          line-clamp: 3;
+          -webkit-box-orient: vertical;
+          color: #222222;
+          font-weight: 500;
+          line-height: 21px;
+          font-size: 14px;
+
+          &:has(+ .read-more:checked) {
+            -webkit-line-clamp: unset
+          }
+        }
+
+        .read-more {
+          margin-top: 3px;
+          color: #BABABA;
+          font-size: 14px;
+          font-weight: 500;
+          line-height: 21px;
+          appearance: none;
+          cursor: pointer;
+
+          &::before {
+            content: "더보기";
+          }
+
+          &:checked::before {
+            content: "닫기";
+          }
+        }
+      }
+
+      .third-row {
+        padding-top: 40px;
+
+        .line {
+          display: flex;
+          justify-content: space-between;
+          height: 78px;
+          margin-bottom: 12px;
+
+          .box {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            width: 124px;
+            height: 100%;
+            border: 1px solid #f2f2f2;
+
+            .filter-name {
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              width: 108px;
+              height: 24px;
+              font-size: 10px;
+              font-weight: 500;
+              line-height: 12px;
+              color: #aaaaaa;
+            }
+
+            .filter-value {
+              padding-top: 4px;
+              width: 108px;
+              height: 24px;
+              text-align: center;
+              font-weight: 700;
+              font-size: 20px;
+              line-height: 24px;
+              color: #222222;
+            }
+          }
+        }
+      }
+
+      .fourth-row {
+        padding-top: 50px;
+        display: flex;
+        justify-content: space-between;
+
+        .content-left {
+          width: 341px;
+
+          .major-title {
+            font-weight: 700;
+            font-size: 14px;
+            color: #222222;
+            margin-bottom: 12px;
+          }
+
+          .major-dropdown {
+            position: relative;
+            cursor: pointer;
+            border: 1px solid #f2f2f2;
+            border-radius: 4px;
+            width: 100%;
+            height: 44px;
+            color: white;
+
+            .current {
+              display: flex;
+              align-items: center;
+              width: 100%;
+              height: 100%;
+
+              p {
+                font-family: 'pretendard';
+                padding-left: 10px;
+                font-size: 16px;
+                color: #222222;
+                line-height: 19px;
+                font-weight: 700;
+              }
+            }
+
+            .dropdown-content {
+              position: fixed;
+              z-index: 1;
+              width: 341px;
+              height: 200px;
+              overflow-y: scroll;
+              border: 1px solid #F2F2F2;
+              border-radius: 0 0 4px 4px;
+
+              .option {
+                font-family: 'pretendard';
+                font-weight: 700;
+                font-size: 16px;
+                line-height: 19px;
+                background: white;
+                padding: 0 10px;
+                display: flex;
+                align-items: center;
+                width: 100%;
+                height: 39px;
+                color: #222222;
+                transition: 0.2s;
+
+                &:hover {
+                  background-color: #82E1FF;
+                }
+              }
+            }
+
+            .icon {
+              pointer-events: none;
+              position: absolute;
+              display: block;
+              top: 50%;
+              transform: translateY(-50%);
+              right: 0;
+              color: black;
+              font-size: 35px;
+            }
+          }
+        }
+
+        .content-right {
+          display: flex;
+          width: 288px;
+
+          .wrapper {
+            width: 139px;
+
+            &:nth-child(1) {
+              margin-right: 10px;
+            }
+
+            .title {
+              font-weight: 700;
+              font-size: 14px;
+              line-height: 17px;
+              color: #222222;
+              margin-bottom: 12px;
+            }
+
+            .value {
+              display: flex;
+              align-items: center;
+              width: 100%;
+              height: 44px;
+              border-radius: 4px;
+              border: 1px solid #f2f2f2;
+
+              span {
+                padding-left: 10px;
+                color: #222222;
+                font-weight: 700;
+                font-size: 16px;
+                line-height: 19px;
+              }
+            }
+          }
+        }
+      }
     }
 
     .modal-dim {
@@ -232,9 +611,10 @@ export default {
         position: relative;
         display: flex;
         justify-content: space-between;
-        width: 850px;
+        width: 100%;
 
         .filter {
+          position: relative;
           display: flex;
           cursor: pointer;
           font-family: 'pretendard';
@@ -242,63 +622,67 @@ export default {
           font-size: 14px;
           color: #222222;
           line-height: 17px;
+          width: max-content;
+
+          .border {
+            position: relative;
+            width: 100%;
+            padding: 5px 10px;
+            border: 1px solid #F2F2F2;
+            border-radius: 4px;
+
+            &.isActive::before {
+              position: absolute;
+              content: url('@/assets/images/Component11.png');
+              top: 7px;
+              right: 10px;
+              width: 10px;
+              height: 11px;
+            }
+
+            &::before {
+              position: absolute;
+              content: url('@/assets/images/Component10.png');
+              top: 7px;
+              right: 10px;
+              width: 10px;
+              height: 11px;
+
+            }
+
+            &.name {
+              width: 80px;
+            }
+          }
+
 
           &.rank {
-            justify-content: center;
-            width: 65px;
-
-            .icon {
-              color: #FF7A00;
-            }
+            width: 80px;
           }
-
-          &.tuition {
-            justify-content: center;
-          }
-
-          &.comp {}
 
           &.name {
-            width: 250px;
+            width: 280px;
+          }
 
-            .icon {
-              color: #82E1FF;
-            }
+          &.comp {
+            width: 120px;
           }
 
           &.num {
-            justify-content: center;
+            width: 120px;
           }
 
-          .icon {
-            margin: 0 1px;
-            font-size: 13px;
-            color: #222222;
-
-            &.rotate {
-              transform: rotate(180deg);
-            }
+          &.sf-ratio {
+            width: 120px;
           }
 
-          &:nth-child(1) {}
-
-          &:nth-child(2) {}
-
-          &:nth-child(3) {
-            width: 80px;
+          &.tuition {
+            width: 120px;
           }
 
-          &:nth-child(4) {
-            width: 80px;
-          }
 
-          &:nth-child(5) {
-            width: 100px;
-          }
 
-          &:nth-child(6) {
-            width: 95px;
-          }
+
         }
       }
     }
@@ -329,12 +713,12 @@ export default {
         display: flex;
         align-items: center;
         justify-content: space-between;
-        width: 850px;
+        width: 100%;
         height: 100%;
 
         .rank {
-          padding-left: 20px;
-          width: 65px;
+          text-align: center;
+          width: 80px;
           font-family: 'pretendard';
           font-weight: 700;
           font-size: 16px;
@@ -344,7 +728,7 @@ export default {
 
         .university {
           display: flex;
-          width: 250px;
+          width: 280px;
 
           .logo {
             width: 40px;
@@ -381,9 +765,8 @@ export default {
           font-weight: 700;
           line-height: 19px;
           color: #222222;
-          width: 80px;
+          width: 120px;
           text-align: center;
-          text-indent: -20px;
         }
 
         .totStud {
@@ -392,7 +775,7 @@ export default {
           font-weight: 700;
           line-height: 19px;
           color: #222222;
-          width: 80px;
+          width: 120px;
           text-align: center;
 
         }
@@ -403,7 +786,7 @@ export default {
           font-weight: 700;
           line-height: 19px;
           color: #222222;
-          width: 100px;
+          width: 120px;
           text-align: center;
 
         }
@@ -414,7 +797,7 @@ export default {
           font-weight: 700;
           line-height: 19px;
           color: #222222;
-          width: 95px;
+          width: 120px;
           text-align: center;
 
         }
